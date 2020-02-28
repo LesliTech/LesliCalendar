b30d19c0f2cd2557dc084d17642a8d7be14a20f4
Luis Donis <ldonis.emc2@gmail.com>
2020-02-27 19:37:15 -0600

    add today events component

---
+++ app/vue/calendars/components/today.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    
+}
+</script>
+<template>
+    <section class="component-driver-calendar-today">
+        <article class="panel">
+            <p class="panel-heading">
+                Today
+            </p>
+            <a class="panel-block" v-for="(event, index) in [0,0,0,0,0,0,0,0,0]" :key="index">
+                <b>{{ index + 8}}:00</b>
+                Besuch Kooperationspartner
+            </a>
+        </article>
+    </section>
+</template>
