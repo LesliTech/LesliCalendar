21c1fd40dd230082759527c71b9891f0c148b653
Luis Donis <ldonis.emc2@gmail.com>
2020-03-19 20:18:32 -0600

    add updates from development

--- app/controllers/cloud_driver/event/files_controller.rb
+++ app/controllers/cloud_driver/event/files_controller.rb
@@ -4,29 +4,4 @@ module CloudDriver
     class Event::FilesController < CloudObject::FilesController
     end
 end
-=begin
-    class Event::FilesController < ApplicationController
-        def create
-            begin
-                file = Event::File.new (event_file_params)
-                file.event = Event.find(1)
-                #file.file.attach(event_file_params[:file])
-                file.save
-            rescue => exception
-                p "~   ~   ~   ~   ~   ~   ~   ~   ~   ~   ~"
-                p exception
-                p "~   ~   ~   ~   ~   ~   ~   ~   ~   ~   ~"    
-            end
-            
-            
-            
-        end
-
-        private
-
-        def event_file_params
-            params.fetch(:event_file).permit(:name, :file)
-        end
-    end
-=end
 
--- app/models/cloud_driver/event/file.rb
+++ app/models/cloud_driver/event/file.rb
@@ -1,5 +1,7 @@
 module CloudDriver
+    #class Event::File < ApplicationRecord
     class Event::File < CloudObject::File
         belongs_to :cloud_object, class_name: "CloudDriver::Event", foreign_key: "cloud_driver_events_id"
+        #mount_uploader :attachment, FileUploader
     end
 end
