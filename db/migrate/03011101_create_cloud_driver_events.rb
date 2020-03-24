d067301bfc610f8053d58022ceb1e31d2d4667c0
diego-alay <dalay@lomax.com.gt>
2020-03-24 11:53:17 -0600

    polymorphic structure for events

--- db/migrate/03011101_create_cloud_driver_events.rb
+++ db/migrate/03011101_create_cloud_driver_events.rb
@@ -1,10 +1,13 @@
 class CreateCloudDriverEvents < ActiveRecord::Migration[6.0]
     def change
         create_table :cloud_driver_events do |t|
-            t.string    :reference_name
-            t.integer   :reference_id
+
             t.timestamps
         end
+        add_reference :cloud_driver_events, :users, foreign_key: true, index: { name: "house_events_users" }
         add_reference :cloud_driver_events, :cloud_driver_calendars, foreign_key: true
+        add_reference :cloud_driver_events, :accounts, foreign_key: true
+        add_reference :cloud_driver_events, :model, polymorphic: true
+        add_reference :cloud_driver_events, :organizer
     end
 end
