67fa9335c6f439eb4bed8e2d4032e695ea65604a
volant360 <efrainh12@galileo.edu>
2020-06-17 19:46:41 +0200

    add trello/1442 belongs association to deleted workflows

--- app/models/cloud_driver/workflow/action.rb
+++ app/models/cloud_driver/workflow/action.rb
@@ -26,6 +26,7 @@ Building a better future, one line of code at a time.
 =end
     class Workflow::Action < CloudObject::Workflow::Action
         belongs_to :workflow, class_name: "Workflow", foreign_key: "cloud_driver_workflows_id"
+        belongs_to :workflow_including_deleted, class_name: "Workflow", foreign_key: "cloud_driver_workflows_id", with_deleted: true
         belongs_to :initial_status, class_name: "Workflow::Status", foreign_key: "initial_status_id", optional: true
         belongs_to :final_status, class_name: "Workflow::Status", foreign_key: "final_status_id"
 
