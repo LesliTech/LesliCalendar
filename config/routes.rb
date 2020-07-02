f2d0a17cdef0d954b49585b11f981c79a923b81b
volant360 <efrainh12@galileo.edu>
2020-07-02 13:57:59 -0600

    fix trello/1546 prefix for customer actions set as 'resource'

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ CloudDriver::Engine.routes.draw do
         collection do
             post "list" => :index
             get  "associations/options",    to: "workflow/associations#options"
+            get "/resource/transition-options/:cloud_object_name/:cloud_object_id", to: "workflows#transition_options"
         end
         scope module: :workflow do
             resources :associations
@@ -27,13 +28,13 @@ CloudDriver::Engine.routes.draw do
 
     resources :events do
         member do
-            get "/custom/files-zip-download",   to:  "event/files#zip_download"
+            get "/resource/files-zip-download",    to:  "event/files#zip_download"
         end
         collection do
             get :search
             get :options 
-            get "/files/options",               to: "event/files#options"
-            get "/custom/events-by-model/:model_type/:model_id" =>  :events_by_model
+            get "/files/options",                   to: "event/files#options"
+            get "/resource/events-by-model/:model_type/:model_id" =>  :events_by_model
         end
         scope module: :event do
             resources :actions
