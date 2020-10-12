24dd2726b2aff1bb498dfe5858f34cb3d46c9a74
Carlos Hermosilla <efrainh12@galileo.edu>
2020-10-12 13:06:48 -0600

    add trello/1858 default dashboard initialization on account creation

--- app/models/cloud_driver/dashboard/component.rb
+++ app/models/cloud_driver/dashboard/component.rb
@@ -1,4 +1,5 @@
 module CloudDriver
-  class Dashboard::Component < ApplicationRecord
-  end
+    class Dashboard::Component < CloudObject::Dashboard::Component
+        belongs_to :dashboard, inverse_of: :components, class_name: "Dashboard", foreign_key: "cloud_driver_dashboards_id"
+    end
 end
