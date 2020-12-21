38f333ade7cc56e0203fcca77b52df21836e41f9
Carlos Hermosilla <efrainh12@galileo.edu>
2020-12-21 10:16:11 -0600

    fix trello/2105 updated license in all files

--- app/models/cloud_driver/event/file.rb
+++ app/models/cloud_driver/event/file.rb
@@ -1,3 +1,21 @@
+=begin
+
+Copyright (c) 2020, all rights reserved.
+
+All the information provided by this platform is protected by international laws related  to 
+industrial property, intellectual property, copyright and relative international laws. 
+All intellectual or industrial property rights of the code, texts, trade mark, design, 
+pictures and any other information belongs to the owner of this platform.
+
+Without the written permission of the owner, any replication, modification,
+transmission, publication is strictly forbidden.
+
+For more information read the license file including with this software.
+
+// · ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
+// · 
+
+=end
 module CloudDriver
     class Event::File < CloudObject::File
         belongs_to :cloud_object, class_name: "CloudDriver::Event", foreign_key: "cloud_driver_events_id"
