1fa2a161698a13b7601377d913d81f72116f53d8
Luis Donis <ldonis.emc2@gmail.com>
2021-06-08 10:21:47 -0600

    feat: implement vue single app per engine

--- lib/cloud_driver/version.rb
+++ lib/cloud_driver/version.rb
@@ -1,9 +1,4 @@
 module CloudDriver
-<<<<<<< HEAD
-  VERSION = '0.0.13'
-    BUILD = "20210604.1327"
-=======
-  VERSION = '0.0.16'
+    VERSION = '0.0.16'
     BUILD = "20210305.0959"
->>>>>>> 28e71228d42c286438c9b5eb2cc0a3f765d0127d
 end
